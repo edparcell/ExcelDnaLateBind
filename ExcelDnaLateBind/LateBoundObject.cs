79f4a9d89e8c67cc89b1ff3dcc217004594628d7
Ed Parcell <ed.parcell@cobaltquantware.com>
2010-09-24 14:52:01 +0100

    LateBoundObject to public. Include reference to ExcelDna.Integration.

--- ExcelDnaLateBind/LateBoundObject.cs
+++ ExcelDnaLateBind/LateBoundObject.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 
 namespace CobaltQuantware.ExcelDnaLateBind
 {
-    internal class LateBoundObject
+    public class LateBoundObject
     {
         protected Type ObjType { get; private set; }
         protected object Obj { get; private set; }
